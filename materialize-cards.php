e2125eb94bf2df6124e68fc8f9e0ccdae821b35e
aj <aj@icannhas.com>
2018-03-23 17:27:25 +0800

    Initial commit

---
+++ materialize-cards.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Plugin Name: Materialize Cards
+ * Description: Display images with captions as Materialize Cards
+ * Plugin URI:  https://github.com/ajatamayo/materialize-cards
+ * Version:     1.0
+ * Author:      AJ Tamayo
+ * Author URI:  https://github.com/ajatamayo
+ * License:     GPL
+ * Text Domain: materialize-cards
+ * Domain Path: /languages
+ *
+ */
+
+add_action( 'plugins_loaded', array( Materialize_Cards::get_instance(), 'plugin_setup' ) );
+
+class Materialize_Cards {
+    protected static $instance = NULL;
+    public $caption_sentinel = 'materialize-cards-caption-sentinel';
+    public $plugin_url = '';
+    public $plugin_path = '';
+
+    /**
+     *
+     * @since 1.0
+     */
+    public function __construct() {}
+
+    /**
+     *
+     * @since 1.0
+     */
+    public function load_language( $domain ) {
+        load_plugin_textdomain(
+            $domain,
+            FALSE,
+            $this->plugin_path . '/languages'
+        );
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    public static function get_instance() {
+        NULL === self::$instance and self::$instance = new self;
+        return self::$instance;
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    public function plugin_setup() {
+        $this->plugin_url    = plugins_url( '/', __FILE__ );
+        $this->plugin_path   = plugin_dir_path( __FILE__ );
+        $this->load_language( 'materialize-cards' );
+
+        // Change caption shortcode to use cards
+        add_filter( 'img_caption_shortcode', array( &$this, 'convert_to_image_card' ), 10, 3 );
+        add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_scripts' ), 10 );
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    function convert_to_image_card( $output, $attr, $content ) {
+        ob_start();
+
+        ?>
+
+        <div class="card">
+            <div class="card-image">
+                <?php echo $content; ?>
+            </div>
+            <div class="card-content">
+                <p><?php echo $attr['caption']; ?></p>
+            </div>
+        </div>
+
+        <?php
+
+        return ob_get_clean();
+    }
+
+    /**
+     *
+     * @since 1.0
+     */
+    function enqueue_scripts() {
+        wp_enqueue_style( 'materialize-cards', $this->plugin_url . "/public/style.css", array(), '1.0' );
+    }
+}
+
+?>
